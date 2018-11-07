78ac260aff0ca595b2523a5cd7de9e487fc1ba4d
modnite <me@modnite.net>
2018-11-06 21:37:19 -0500

    Added 7 new endpoints for events and friends system

---
+++ ModniteServer.Api/Controllers/FriendsController.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModniteServer.API.Controllers
+{
+    public sealed class FriendsController : Controller
+    {
+        [Route("GET", "/friends/api/public/friends/*")]
+        public void GetFriends()
+        {
+            string accountId = Request.Url.Segments.Last();
+
+            var response = new List<object>();
+
+            Response.StatusCode = 200;
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(response));
+        }
+
+        [Route("GET", "/friends/api/public/blocklist/*")]
+        public void GetBlocklist()
+        {
+            string accountId = Request.Url.Segments.Last();
+
+            var response = new
+            {
+                blockedUsers = new List<object>()
+            };
+
+            Response.StatusCode = 200;
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(response));
+        }
+
+        [Route("GET", "/friends/api/public/list/fortnite/*/recentPlayers")]
+        public void GetRecentPlayers()
+        {
+            string accountId = Request.Url.Segments[Request.Url.Segments.Length - 2].Replace("/", "");
+
+            var response = new List<object>();
+
+            Response.StatusCode = 200;
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(response));
+        }
+
+        [Route("GET", "/friends/api/v1/*/settings")]
+        public void GetSettings()
+        {
+            string accountId = Request.Url.Segments[Request.Url.Segments.Length - 2].Replace("/", "");
+
+            var response = new
+            {
+                acceptInvites = "public"
+            };
+
+            Response.StatusCode = 200;
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(response));
+        }
+
+        [Route("GET", "/account/api/public/account/displayName/*")]
+        public void FindFriendByDisplayName()
+        {
+            string displayName = Request.Url.Segments.Last();
+            string type = Request.Url.Segments[Request.Url.Segments.Length - 2].Replace("/", "");
+
+            var response = new
+            {
+                errorCode = "errors.com.epicgames.account.account_not_found",
+                errorMessage = "Sorry, we couldn't find an account for " + displayName,
+                messageVars = new List<string> { displayName },
+                numericErrorCode = 18007,
+                originatingService = "com.epicgames.account.public",
+                intent = "prod"
+            };
+
+            Response.StatusCode = 404;
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(response));
+        }
+
+        [Route("GET", "/account/api/public/account/email/*")]
+        public void FindFriendByEmail()
+        {
+            string email = Request.Url.Segments.Last();
+
+            var response = new
+            {
+                errorCode = "errors.com.epicgames.account.account_not_found",
+                errorMessage = "Sorry, we couldn't find an account for " + email,
+                messageVars = new List<string> { email },
+                numericErrorCode = 18007,
+                originatingService = "com.epicgames.account.public",
+                intent = "prod"
+            };
+
+            Response.StatusCode = 404;
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(response));
+        }
+    }
+}
---
+++ ModniteServer.Api/Controllers/TournamentController.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModniteServer.API.Controllers
+{
+    public sealed class TournamentController : Controller
+    {
+        [Route("GET", "/fortnite/api/game/v2/events/tournamentandhistory/*/*/*")]
+        public void GetTournaments()
+        {
+            string accountId = Request.Url.Segments[Request.Url.Segments.Length - 3].Replace("/", "");
+            string region = Request.Url.Segments[Request.Url.Segments.Length - 2].Replace("/", "");
+            string clientType = Request.Url.Segments.Last();
+
+            var response = new
+            {
+                eventTournaments = new List<object>(),
+                eventWindowHistories = new List<object>()
+            };
+
+            Response.StatusCode = 200;
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(response));
+        }
+    }
+}
